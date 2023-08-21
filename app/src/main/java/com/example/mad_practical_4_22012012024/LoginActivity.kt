0526d2130cf8b0fcaa682de4120fe400ad7ff3ea
smit1 <Smit02042004@gmail.com>
2023-08-21 14:39:44 +0530

    practical-4 complate

--- app/src/main/java/com/example/mad_practical_4_22012012024/LoginActivity.kt
+++ app/src/main/java/com/example/mad_practical_4_22012012024/LoginActivity.kt
@@ -1,11 +1,17 @@
 package com.example.mad_practical_4_22012012024
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        val signUp: Button =findViewById(R.id.buttonSignUp)
+        signUp.setOnClickListener {
+            Intent(this@LoginActivity,RegistrationActivity::class.java).also { startActivity(it) }
+        }
     }
 }
